f1252982d9d3112db87ca9ea80653ac48a4b5e83
Bruno Buck <b.brigagao@gmail.com>
2022-02-11 13:02:38 -0300

    create connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
 import express from "express";
+import { createConnection } from "typeorm";
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => console.log("Server is running."));
+
+createConnection().then(async connection => {
+    console.log("connected...");
+}).catch(error => console.log(error));
