d0cba082855fd9b7222976e5c8df5569343066c8
Bruno Buck <b.brigagao@gmail.com>
2022-09-27 18:47:14 -0300

    dummy test

---
+++ src/test/category.test.js
@@ -0,0 +1,17 @@
+const { request, response } = require( "express" );
+
+/* describe( "Test the root path", () => {
+    test( "It should response the GET method", () => {
+        return request( app )
+            .get( "/" )
+            .then( response => {
+                expect( response.statusCode ).toBe( 200 );
+            } );
+    } );
+} ); */
+
+describe( "Basic test", () => {
+    test( "It should do nothing", () => {
+        return undefined;
+    })
+})
